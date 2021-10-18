38049fb8c41d9091775e195bce9ae6d488a9c438
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-10-18 09:59:03 +0700

    login API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,18 +2,46 @@
 
 namespace App\Http\Controllers\API;
 
+use Exception;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     //
     public function login(Request $request){
         try{
+            // validasi input
             $request->validate([
                 'email'=>'email|required',
             ]);
+            //Check kredensial login
+            $crdentials = request(['email','password']);
+            if(!Auth::attempt($crdentials)){
+                return ResponseFormatter::error([
+                    'message'=>'Unauthorized'
+                ],'Authentication Failed',500);
+            }
+            //jika tiak berhasil maka throw error
+            $user = User::where('email',$request->email)->first();
+            if(!Hash::check($request->password,$user->password,[])){
+                throw new \Exception('Invalid Credentials');
+            }
+
+            // jika berhasil maka buat token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type'=> 'Bearer',
+                'user' =>$user
+            ],'Authenticated');
         }
-        catch{
+        catch(Exception $error){
+            return  ResponseFormatter::error([
+                'message'=> 'Something wnt wrong',
+                'error'=> $error
+            ],'Auth Failed',500);
 
         }
         finally{
